a289e8049a8b9c551c8286d7eb09bf6d679382d5
DMLhope <605811524@qq.com>
2020-06-29 18:27:52 +0800

    channels

---
+++ non-blocking-channel-operations/main.go
@@ -0,0 +1,18 @@
+//非阻塞通道操作
+package main
+
+import "fmt"
+
+func main() {
+	messages := make(chan string)
+	signals := make(chan bool)
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	default:
+		fmt.Println("no message received")
+	}
+
+	msg := "hi"
+}
